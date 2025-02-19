e5bf7399936c04b23700dc9f3dbc851913c9a1fc
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-02-19 19:16:51 -0300

    feat: Subindo o Redis e configurando a aplicação para cache

--- src/main/java/com/cursochat/ws/WsApplication.java
+++ src/main/java/com/cursochat/ws/WsApplication.java
@@ -2,7 +2,9 @@ package com.cursochat.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class WsApplication {
 
